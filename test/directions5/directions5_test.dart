368d89d85b37e310b755dad535feea03bba612a8
bamnenim <bamnenim@gmail.com>
2020-04-18 22:18:51 +0900

    b4 removing impls

--- test/directions5/directions5_test.dart
+++ test/directions5/directions5_test.dart
@@ -1,5 +1,31 @@
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  
+
+  setUp(() {
+
+  });
+  group('this is test for directions5! ', () {
+    group('request call tests: ', () {
+      test('should return response object when the call is successful', 
+      () async {
+        ///arrange
+        
+        ///act
+        
+        ///assert
+        
+      });
+
+      test('should return response object when the call is unsuccessful', 
+      () async {
+        ///arrange
+        
+        ///act
+        
+        ///assert
+        
+      });
+    });
+  });
 }
